9758f5bfcb00749ac60e35b14ce3a230e731c256
sling254 <mwanikidenis7@gmail.com>
2021-10-22 17:22:59 +0300

    add a test to check display_credentials method

--- Credentials/test_credentials.py
+++ Credentials/test_credentials.py
@@ -84,7 +84,11 @@ class TestCredential(unittest.TestCase):
         self.assertTrue(credential_exists)
 
     
-   
+    def test_display_all_credentials(self):
+        '''
+        method that returns a list of all credentials saved
+        '''
+        self.assertEqual(Credential.display_credentials(),Credential.credential_list)
 
 
 
