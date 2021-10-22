5342990c3bce6c243d4164119804e6dfd3e14f74
sling254 <mwanikidenis7@gmail.com>
2021-10-22 16:57:47 +0300

    add test for delete credentials method

--- Credentials/test_credentials.py
+++ Credentials/test_credentials.py
@@ -45,6 +45,17 @@ class TestCredential(unittest.TestCase):
         test_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),2)
 
+    def test_delete_credential(self):
+        '''
+        test_delete_credential to test if we can remove a credential from our credential list
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credential("NickKorgoren","Better00","0714042437") #new credential
+        test_credential.save_credential()
+
+        self.new_credential.delete_credential() #Deleting a credential object
+        self.assertEqual(len(Credential.credential_list),1)
+
         
 
 
